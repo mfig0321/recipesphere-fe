01884e3b3eafca1371614c1d993808f1b62e3a9f
Omar <omarsufi39@hotmail.com>
2023-08-07 20:20:17 -0700

    Created the initial react app for recipesphere

---
+++ recipesphere-frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is the webpage for recipesphere</h1>
+    </div>
+  );
+}
+
+export default App;
