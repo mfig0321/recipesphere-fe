d65b717cd604c2ba8dcd72adb69893580b94ef0d
Omar <omarsufi39@hotmail.com>
2023-08-12 15:38:24 -0700

    Removed unnecessary files and added recipe cards and logos to frontpage

--- recipesphere-frontend/src/App.js
+++ recipesphere-frontend/src/App.js
@@ -1,12 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import homepageLogo from "./images/ReceipeSpehereLogov2.PNG";
+
+//Compontents
+import Card from './components/card/Card';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>This is the webpage for recipesphere</h1>
+
+      <div className='homepage-logo'>
+        <h1>RecipeSphere</h1>
+        <img src={homepageLogo} alt="Company logo" />
+      </div>
+
+      <div className='card-collection'>
+        <Card 
+        imgsrc="https://picsum.photos/300/200"
+        imgalt="Card Image"
+        title="Card Title"
+        description="This is the Card Description"
+        buttonlink="#"
+        buttontitle="Card Button"
+      />
+
+      <Card 
+        imgsrc="https://picsum.photos/300/200"
+        imgalt="Card Image"
+        title="Card Title"
+        description="This is the Card Description"
+        buttonlink="#"
+        buttontitle="Card Button"
+      />
+
+      <Card 
+        imgsrc="https://picsum.photos/300/200"
+        imgalt="Card Image"
+        title="Card Title"
+        description="This is the Card Description"
+        buttonlink="#"
+        buttontitle="Card Button"
+      />
+
+      <Card 
+        imgsrc="https://picsum.photos/300/200"
+        imgalt="Card Image"
+        title="Card Title"
+        description="This is the Card Description"
+        buttonlink="#"
+        buttontitle="Card Button"
+      />
+
+      <Card 
+        imgsrc="https://picsum.photos/300/200"
+        imgalt="Card Image"
+        title="Card Title"
+        description="This is the Card Description"
+        buttonlink="#"
+        buttontitle="Card Button"
+      />
+
+      <Card 
+        imgsrc="https://picsum.photos/300/200"
+        imgalt="Card Image"
+        title="Card Title"
+        description="This is the Card Description"
+        buttonlink="#"
+        buttontitle="Card Button"
+      />
+      </div>
+      
+      
     </div>
   );
 }
 
 export default App;
+
+
+
+
